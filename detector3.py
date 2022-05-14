8fe76ed91a18a284615428b40114d4b4fdcf72d1
igor.sondors <igor.sondors@gmail.com>
2022-05-14 23:13:19 +0000

    mediapipe

--- detector3.py
+++ detector3.py
@@ -1,22 +1,20 @@
+import mediapipe as mp
 import pandas as pd
 import numpy as np
-import os
 import click
 import time
 import cv2
-import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_face_mesh = mp.solutions.face_mesh
+import os
 
-#frames_df = pd.read_csv('/home/yandex/igor/data_eyes/csv/altyn_frames_podium_const_sep_norm.csv', sep=';')
-#paths = tuple(frames_df['file'])
-paths = ['/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0021.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0012.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0042.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0018.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0014.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0015.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0035.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0061.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0064.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0038.jpg']
+frames_df = pd.read_csv('/home/yandex/igor/data_eyes/csv/altyn_frames_podium_const_sep_norm.csv', sep=';')
+paths = tuple(frames_df['file'])
+#paths = ['/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0021.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0012.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0042.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0018.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0014.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0015.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0035.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0061.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0064.jpg', '/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112/445493eb-d0fa-4670-b785-bb3d2e10082d/0038.jpg']
 @click.command()
 @click.option('--source', default=paths, type=str)
 @click.option('--destination', default='/mnt/data/frames/altyn_original_fps_igor_mediapipe_lndm_npy', type=str)
 def fmain(source, destination):
     i = -1
+    mp_face_mesh = mp.solutions.face_mesh
     shift = len('/mnt/data/frames/altyn_original_fps_igor_origami_extended_0003_112')
     with mp_face_mesh.FaceMesh(
         static_image_mode=True,
@@ -33,7 +31,7 @@ def fmain(source, destination):
 
             # Print and draw face mesh landmarks on the image.
             if not results.multi_face_landmarks:
-                click.echo(f"--> {filename} was processed with error {e}")
+                click.echo(f"--> {filename} was processed with error")
                 continue
 
             #print('facial_landmarks: ', facial_landmarks)
@@ -41,8 +39,6 @@ def fmain(source, destination):
             #click.echo(f"--> Processing {filename}.. SUCCESS")
 
             print('{}: {} ms'.format(i, int((time.time() - start_time)*1000 )))
-
-
 
 if __name__ == '__main__':
     fmain()
